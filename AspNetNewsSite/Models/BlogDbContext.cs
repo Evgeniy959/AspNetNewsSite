bf9f8181b37b6d483801e7830fdccc75341236e1
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-11-13 17:25:47 +0700

    Done

--- AspNetNewsSite/Models/BlogDbContext.cs
+++ AspNetNewsSite/Models/BlogDbContext.cs
@@ -11,25 +11,9 @@ namespace AspNetNewsSite.Models
         public BlogDbContext(DbContextOptions options) : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<PostTag>()
-            //     .HasOne(pt => pt.Post)
-            //     .WithMany(p => p.PostTags)
-            //     .HasForeignKey(pt => pt.PostId);
-
-
-            //modelBuilder.Entity<PostTag>()
-            //     .HasOne(pt => pt.Tag)
-            //     .WithMany(p => p.PostTags)
-            //     .HasForeignKey(pt => pt.TagId);
-        }
-
 
         public DbSet<Person> Persons { get; set; }
         public DbSet<Сomment> Сomments { get; set; }
-        /*public DbSet<Tag> Tags { get; set; }
-        public DbSet<PostTag> PostTags { get; set; }*/
     }
 }
 
